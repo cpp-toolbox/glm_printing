359938f9646b7f50e6c473c63ff0803c8fca2351
cuppajoeman <ccn@cuppajoeman.com>
2025-03-10 05:12:24 -0400

    feat: printing for vec2

--- glm_printing.hpp
+++ glm_printing.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <glm/glm.hpp>
 
+inline std::ostream &operator<<(std::ostream &os, const glm::vec2 &v) { return os << "(" << v.x << ", " << v.y << ")"; }
+
 inline std::ostream &operator<<(std::ostream &os, const glm::vec3 &v) {
     return os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
 }
